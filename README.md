9cc68fce38b57ba701092c8af30e1555fa8682f3
dong <1006991905@qq.com>
2017-11-08 23:17:45 +0800

    test

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
 ##　算法集锦(排序)# algorithms
-# algorithms
-# algorithms
+
---
+++ Sort/quick.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 快速排序
+ * @param array $data
+ * @throws Exception
+ */
+
+function quick(array $data = array())
+{
+    if (empty($data))
+        throw new Exception("数据不能为空");
+    $num = count($data);
+    for ($i = 0; $i < $num; $i++)
+    {
+
+
+    }
+}
